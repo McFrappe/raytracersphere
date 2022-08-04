f7462cde5033edc53dbb860a6e64b8e90407c20c
mcfrappe <fahadrami.jamil.9983@student.uu.se>
2022-08-04 21:32:41 +0200

    changed from ppm to jpg

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ void render(const Sphere &sphere, const Light &light) {
 
 	// save the framebuffer to file
 	std::ofstream ofs;
-	ofs.open("./out.ppm");
+	ofs.open("./out.jpg");
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 	for (size_t i = 0; i < height * width; ++i) {
 		for (size_t j = 0; j<3; j++) {
